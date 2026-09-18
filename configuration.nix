b26f8eb97a2c8b7dbb01c58b8b2b9a1afaa10029
Ron <1757093971@qq.com>
2026-09-18 12:33:16 +0800

    auto: /nix/store/0ws0f4iar1dxrnnj1ibn19i58yz1jhpw-git-sync.service-c2903aa58ccf42b19db59b1935ce55c2-/run/user/1000/credentials/git-sync.service_nixos:

--- configuration.nix
+++ configuration.nix
@@ -19,7 +19,7 @@ let
         pkgs.libsecret pkgs.wayland pkgs.zlib pkgs.stdenv.cc.cc.lib
       ]
     }:$LD_LIBRARY_PATH
-    exec ${pkgs.appimage-run}/bin/appimage-run $HOME/Applications/Motrix-2.0.0-beta.39-x86_64.AppImage "$@"
+    exec ${pkgs.appimage-run}/bin/appimage-run $HOME/nixos-config/appimages/Motrix-2.0.0-beta.39-x86_64.AppImage "$@"
   '';
 in
 {
@@ -44,6 +44,13 @@ in
 
   # Enable networking
   networking.networkmanager.enable = true;
+
+  # Nix-daemon proxy settings
+  systemd.services.nix-daemon.environment = {
+    HTTP_PROXY = "http://127.0.0.1:7897";
+    HTTPS_PROXY = "http://127.0.0.1:7897";
+    ALL_PROXY = "socks://127.0.0.1:7897";
+  };
  
   # Set your time zone.
   time.timeZone = "Asia/Shanghai";
